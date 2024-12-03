5b3d81cf18c4c4ab56c42afdf089ecbbfceed2cf
Leandro Martins <leandro.martins.85@gmail.com>
2024-12-03 18:27:39 -0300

    Generate Notes List component

---
+++ src/components/fsk-notes-list/fsk-notes-list.tsx
@@ -0,0 +1,16 @@
+import { Component, Host, h } from '@stencil/core';
+
+@Component({
+  tag: 'fsk-notes-list',
+  styleUrl: 'fsk-notes-list.css',
+  shadow: true,
+})
+export class FskNotesList {
+  render() {
+    return (
+      <Host>
+        <slot></slot>
+      </Host>
+    );
+  }
+}
