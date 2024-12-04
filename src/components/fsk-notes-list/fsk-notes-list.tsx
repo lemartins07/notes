e1bac6dc72e9bb91676ee2148db5721ea8e65f55
Leandro Martins <leandro.martins.85@gmail.com>
2024-12-03 22:55:55 -0300

    Data in note-list

--- src/components/fsk-notes-list/fsk-notes-list.tsx
+++ src/components/fsk-notes-list/fsk-notes-list.tsx
@@ -8,6 +8,8 @@ import { getList } from '../../library/NotesData';
 })
 export class FskNotesList {
   render() {
+    console.log(getList())
+    const notes = getList();
     return (
       <Host>
         <div>
@@ -15,9 +17,20 @@ export class FskNotesList {
           <table>
             <thead>
               <tr>
-                <th>{getList()}</th>
+                <th>#</th>
+                <th>Date/Time</th>
+                <th>Title</th>
               </tr>
             </thead>
+            <tbody>
+              {notes.map((note: any) => (
+                <tr key={note.id}>
+                  <td>{note.id}</td>
+                  <td>{note.datetime}</td>
+                  <td>{note.title}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </Host>
--- src/library/NotesData.ts
+++ src/library/NotesData.ts
@@ -1,3 +1,12 @@
+const list = `
+  [
+    { "id": "1", "datetime": "2020-03-01T10:10Z", "title": "Title 1" },
+    { "id": "2", "datetime": "2020-04-01T10:10Z", "title": "Title 2" },
+    { "id": "3", "datetime": "2020-05-01T10:10Z", "title": "Title 3" },
+    { "id": "4", "datetime": "2020-06-01T10:10Z", "title": "Title 4" }
+  ]
+`
+
 export function getList() {
-  return 'Hello world!';
+  return JSON.parse(list)
 }
